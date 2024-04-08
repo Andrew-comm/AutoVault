acc93b2f5da689878e567df9399c2a1afed4cc7c
andrea <ndrwrono2001@gmail.com>
2024-04-08 03:46:00 +0300

    updates for test deployement

--- CarPulse/CarPulse/settings.py
+++ CarPulse/CarPulse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^(iqwonf_@#adr!vjb_bpbu!9v-jyhvz8hau=jc9o#*8wzuxa8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['autovault.onrender.com','127.0.0.1','localhost']
 
 
 AUTHENTICATION_BACKENDS = [
